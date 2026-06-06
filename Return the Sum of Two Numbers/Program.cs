38178b618a4855b5402594bd94774e30f3c33d70
Ramykamal0 <ramykamal012@gmail.com>
2026-06-06 18:51:58 +0300

    Add: Return the Sum of Two Numbers

---
+++ Return the Sum of Two Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a function that takes two numbers as arguments and returns their sum.
+
+using System;
+namespace ReturnTheSumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Sum(a, b);
+            
+        }
+        static int Sum(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+
+    }
